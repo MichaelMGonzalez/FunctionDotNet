a9d4d0cab0eb68c35a4deabb0e3aecf76ed50065
MichaelMGonzalez <mmg005@ucsd.edu>
2016-09-18 22:17:47 -0700

    Added preprocessor guards to tester files

--- MathFunctionParser/LexerTester.cs
+++ MathFunctionParser/LexerTester.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+#if NUnit
 using NUnit.Framework;
 
 namespace MathFunctionParser
@@ -82,3 +83,4 @@ namespace MathFunctionParser
         }
     }
 }
+#endif
--- MathFunctionParser/ParserTester.cs
+++ MathFunctionParser/ParserTester.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if NUnit
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -143,3 +144,4 @@ namespace MathFunctionParser
         
     }
 }
+#endif
